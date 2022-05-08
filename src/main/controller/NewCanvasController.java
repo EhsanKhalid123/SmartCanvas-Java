494bc1d50cb3acae812fa6527ac2b1168da5ef30
Ehsan Khalid <s3838975@student.rmit.edu.au>
2022-05-08 12:45:58 +1000

    Changed Directory To MVC Pattern & Some Code Changes & Database Setup Started

---
+++ src/main/controller/NewCanvasController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import dao.UserDao;
+
+public class NewCanvasController {
+    @FXML
+    private TextField canvasLength;
+    @FXML
+    private TextField canvasWidth;
+    @FXML
+    private Button ok;
+
+    public static String length;
+    public static String width;
+
+    @FXML
+    void initialize() {
+    }
+
+    @FXML
+    void newCanvasCancel() {
+        Stage stage = (Stage) ok.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void newCanvasOk() {
+
+        length = canvasLength.getText();
+        width = canvasWidth.getText();
+
+        System.out.println("length: " + length + "Width: " + width);
+
+        if (!length.isEmpty() || !width.isEmpty()) {
+
+            UserDao.pane.setMinHeight(Double.parseDouble(length));
+            UserDao.pane.setMinWidth(Double.parseDouble(width));
+
+            UserDao.canvas.setHeight(Double.parseDouble(length));
+            UserDao.canvas.setWidth(Double.parseDouble(width));
+
+            if (UserDao.pane.getChildren().contains(UserDao.canvas)) {
+                UserDao.pane.getChildren().remove(UserDao.canvas);
+            }
+            UserDao.pane.getChildren().add(UserDao.canvas);
+            UserDao.pane.setStyle("-fx-background-color: white");
+
+            if (UserDao.canvasPane.getChildren().contains(UserDao.pane)) {
+                UserDao.canvasPane.getChildren().remove(UserDao.pane);
+            }
+            UserDao.canvasPane.getChildren().add(UserDao.pane);
+            UserDao.canvasPane.setStyle("-fx-background-color: white");
+        }
+
+        Stage stage = (Stage) ok.getScene().getWindow();
+        stage.close();
+    }
+
+}
---
+++ src/main/module-info.java
@@ -0,0 +1,19 @@
+module A2.Smart.Canvas.Ehsan.Khalid {
+    requires javafx.graphics;
+    requires java.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires java.sql;
+//    requires javafx.swt;
+
+//    opens smartcanvas;
+    opens controller;
+    opens model;
+    opens resources.views;
+    opens dao;
+
+
+}
